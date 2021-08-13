1a32e441f56d53da2de09ce7c2d3be2790020569
Daniel Trevino <23410540+danieltre23@users.noreply.github.com>
2021-08-13 19:39:02 +0000

    refactor(compiler-cli): add abstract `TemplateCheckWithVisitor` class
    Add an abstract class that has a default implementation of the run
    function and visits all nodes. Authors of template checks can extend
    this class and override the `visitNode` method to return diagnostics.
    
    Refs #42966

--- packages/compiler-cli/src/ngtsc/typecheck/extended/api/api.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/extended/api/api.ts
@@ -6,7 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {TmplAstNode} from '@angular/compiler';
+import {AST, RecursiveAstVisitor, TmplAstBoundAttribute, TmplAstBoundEvent, TmplAstBoundText, TmplAstContent, TmplAstElement, TmplAstIcu, TmplAstNode, TmplAstRecursiveVisitor, TmplAstReference, TmplAstTemplate, TmplAstText, TmplAstTextAttribute, TmplAstVariable} from '@angular/compiler';
+import {ASTWithSource} from '@angular/compiler/src/compiler';
 import * as ts from 'typescript';
 
 import {ErrorCode} from '../../../diagnostics';
@@ -42,3 +43,97 @@ export interface TemplateContext {
  * Function to construct a TemplateCheck with a TempalteContext;
  */
 export type TemplateCheckFactory = (ctx: TemplateContext) => TemplateCheck<ErrorCode>;
+
+/**
+ * This abstract class provides a base implementation for the run method.
+ */
+export abstract class TemplateCheckWithVisitor<T extends ErrorCode> implements TemplateCheck<T> {
+  abstract code: T;
+
+  constructor(readonly ctx: TemplateContext) {}
+
+  /**
+   * Base implementation for run function, visits all nodes in template and calls
+   * `visitNode()` for each one.
+   */
+  run(component: ts.ClassDeclaration, template: TmplAstNode[]): NgTemplateDiagnostic<T>[] {
+    const visitor = new CompleteVisitor<T>(this.ctx, this, component)
+    return visitor.getDiagnostics(template);
+  }
+
+  /**
+   * Visit a TmplAstNode or AST node of the template. Authors should override this
+   * method to implement the check and return diagnostics.
+   */
+  abstract visitNode(ctx: TemplateContext, component: ts.ClassDeclaration, node: TmplAstNode|AST):
+      NgTemplateDiagnostic<T>[];
+}
+
+class CompleteVisitor<T extends ErrorCode> extends RecursiveAstVisitor implements
+    TmplAstRecursiveVisitor {
+  diagnostics: NgTemplateDiagnostic<T>[] = [];
+
+  constructor(readonly ctx: TemplateContext, readonly check: TemplateCheckWithVisitor<T>, readonly component: ts.ClassDeclaration) {
+    super();
+  }
+
+  override visit(node: AST|TmplAstNode, context?: any) {
+    this.diagnostics.push(...this.check.visitNode(this.ctx, this.component, node));
+    node.visit(this);
+  }
+
+  visitAllNodes(nodes: TmplAstNode[]) {
+    for (const node of nodes) {
+      this.visit(node);
+    }
+  }
+
+  visitAst(ast: AST) {
+    if (ast instanceof ASTWithSource) {
+      ast = ast.ast;
+    }
+    this.visit(ast);
+  }
+
+  visitElement(element: TmplAstElement) {
+    this.visitAllNodes(element.attributes);
+    this.visitAllNodes(element.inputs);
+    this.visitAllNodes(element.outputs);
+    this.visitAllNodes(element.references);
+    this.visitAllNodes(element.children);
+  }
+
+  visitTemplate(template: TmplAstTemplate) {
+    this.visitAllNodes(template.attributes);
+    if (template.tagName === 'ng-template') {
+      this.visitAllNodes(template.inputs);
+      this.visitAllNodes(template.outputs);
+      this.visitAllNodes(template.templateAttrs);
+    }
+    this.visitAllNodes(template.variables);
+    this.visitAllNodes(template.references);
+    this.visitAllNodes(template.children);
+  }
+  visitContent(content: TmplAstContent): void {}
+  visitVariable(variable: TmplAstVariable): void {}
+  visitReference(reference: TmplAstReference): void {}
+  visitTextAttribute(attribute: TmplAstTextAttribute): void {}
+  visitBoundAttribute(attribute: TmplAstBoundAttribute): void {
+    this.visitAst(attribute.value);
+  }
+  visitBoundEvent(attribute: TmplAstBoundEvent): void {
+    this.visitAst(attribute.handler);
+  }
+  visitText(text: TmplAstText): void {}
+  visitBoundText(text: TmplAstBoundText): void {
+    this.visitAst(text.value);
+  }
+  visitIcu(icu: TmplAstIcu): void {}
+
+  getDiagnostics(template: TmplAstNode[]):
+      NgTemplateDiagnostic<T>[] {
+    this.diagnostics = [];
+    this.visitAllNodes(template);
+    return this.diagnostics;
+  }
+}
